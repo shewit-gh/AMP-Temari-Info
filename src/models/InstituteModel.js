f58eb3e8eea14cba0289e8580b1c0ef66cfa3162
shewit <se.shewit.ghiwot@gmail.com>
2021-08-30 01:07:38 -0700

    institute model updated

---
+++ src/models/InstituteModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const instSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    institute_name:{
+        type: String,
+        required: true
+    },
+    phone_no:{
+        type: String,
+        required: false,
+    },
+    email:{
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    university_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Univresity",
+        required:true
+    },
+    department_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Department",
+        required:true
+    }
+
+});
+
+
+module.exports = mongoose.model('institute', instSchema);
