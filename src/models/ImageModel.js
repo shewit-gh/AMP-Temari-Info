0b3aaeee01c7cba1e563a0de5fcb81280c2f61ef
shewit <se.shewit.ghiwot@gmail.com>
2021-08-31 06:43:28 -0700

    images model updated

--- src/models/ImageModel.js
+++ src/models/ImageModel.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+
+const imageModel = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    image_name:{
+        type: String,
+        required: true
+    },
+    university_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UNiversity',
+        required: true
+    }
+  
+
+});
+
+module.exports = mongoose.model('Image', imageModel);
