51b20bea7f8f96f91735465a3f07688a95d96d24
shewit <se.shewit.ghiwot@gmail.com>
2021-09-01 07:31:07 -0700

    Institute route and controller updated

--- src/routes/InstituteRoute.js
+++ src/routes/InstituteRoute.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var instController = require('../controllers/InstituteController');
 
-router.get('/', );
+router.get('/', instController.getAll);
+router.get('/:instId', instController.getOne);
+router.delete('/:instId', instController.delete);
+router.post('/', instController.post);
+router.patch('/:id', instController.patch);
 
 module.exports = router;
