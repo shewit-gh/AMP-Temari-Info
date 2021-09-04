edac5b370050cbffe0b2fae2d324ee92659f7358
Blen92 <se.blen.ali92@gmail.com>
2021-09-04 04:07:24 +0300

    appBar and bottom navigation added

--- temari_info_flutter/lib/presentation/home/home_screen.dart
+++ temari_info_flutter/lib/presentation/home/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-// import 'package:temari_info_flutter/presentation/shared/navBar_Widget.dart';
+import 'package:temari_info_flutter/presentation/shared/navBar_Widget.dart';
 
 class Home extends StatelessWidget {
   static const String routeName = "/home";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NavBar(),
+    );
   }
 }
--- temari_info_flutter/lib/presentation/shared/navBar_Widget.dart
+++ temari_info_flutter/lib/presentation/shared/navBar_Widget.dart
@@ -1,1 +1,65 @@
+import 'package:flutter/material.dart';
 
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  _BasicBottomNavBarState createState() => _BasicBottomNavBarState();
+}
+
+class _BasicBottomNavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        leading: Icon(Icons.menu),
+        title: Text(
+          'ተማሪ info',
+          style: TextStyle(
+            color: Color.fromRGBO(118, 219, 224, 1),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.search),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black12,
+        selectedItemColor: Color.fromRGBO(118, 219, 224, 1),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
