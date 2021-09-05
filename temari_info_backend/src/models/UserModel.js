4fbadc82cf15089968d95be61b13b5a8618f91c0
Edenmt301 <se.eden.mesfin@gmail.com>
2021-09-05 22:32:54 +0300

    setup authentication routes and their controllers

--- temari_info_backend/src/models/UserModel.js
+++ temari_info_backend/src/models/UserModel.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const userSchema=mongoose.Schema({
 
-    _id:mongoose.Schema.Types.objectId,
+    _id:mongoose.Schema.Types.ObjectId,
     username:{
         type: String,
         required: true,
@@ -12,24 +12,28 @@ const userSchema=mongoose.Schema({
         required: true,
     },
     password:{
-        type:mongoose.Schema.Types.objectId,
+        type:String,
         required: true,
     },
     role:{
         type:String,
         required: true,
     },
+    token:{
+        type:String
+    },
     comments:[ 
         {
-           type: mongoose.Schema.Types.objectId,
+           type: mongoose.Schema.Types.ObjectId,
            ref :"Comment",
            required:false,
         },
     ],
     rating_id:{
-        type:mongoose.Schema.Types.objectId,
-        ref:"Rating"
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Rating",
+        required:false,
     }
 
-});
+},{strict: false});
 module.exports = mongoose.model('User', userSchema);
