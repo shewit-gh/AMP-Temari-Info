dd1e8e68680d6d529b5515e4dda15918cafe92ef
shewit <se.shewit.ghiwot@gmail.com>
2021-09-06 12:56:43 -0700

    staged un staged changes

--- temari_info_backend/src/controllers/InstituteController.js
+++ temari_info_backend/src/controllers/InstituteController.js
@@ -51,7 +51,7 @@ module.exports ={
     delete: (req, res)=>{
         const id = req.params.instId;
         const inst = Institute.find({_id: id})
-        University.updateOne({_id: isnt.univ_id}, {$pull:{institute: inst._id }}).exec();
+        University.updateOne({_id: inst.univ_id}, {$pull:{institute: [inst._id] }}).exec();
         Institute.remove({_id: id}).exec()
         .then(result => {
             if(result){
