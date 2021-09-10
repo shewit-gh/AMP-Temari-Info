39ff48d36bec735c4cfd01e6c98e72982c779bd1
Edenmt301 <se.eden.mesfin@gmail.com>
2021-09-10 12:17:19 +0300

    fix signup screen

---
+++ temari_info_flutter/lib/auth/bloc/new_auth_bloc.dart
@@ -0,0 +1,105 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:temari_info_flutter/auth/repository/auth_repo.dart';
+import 'package:temari_info_flutter/auth/bloc/auth_event.dart';
+import 'package:temari_info_flutter/auth/bloc/auth_state.dart';
+import 'package:temari_info_flutter/auth/models/auth_model.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository authRepository;
+  AuthBloc({required this.authRepository}) : super(SignedOut());
+
+  @override
+  Stream<AuthState> mapEventToState(AuthEvent event) async* {
+    if (event is SignInEvent) {
+      final User user = event.user;
+      print(user);
+      // reaching to the backend
+      yield SigninInprogress();
+      final response = await authRepository.signIn(user);
+
+      if (response.value == "Failure") {
+        yield AuthFailed(errorMsg: 'Failed to Sign in');
+        await Future.delayed(Duration(seconds: 2));
+           yield SignedOut();
+      }
+      if (response.value == "Invalid Credentials") {
+        yield AuthFailed(errorMsg: 'Incorrect Username or password');
+        await Future.delayed(Duration(seconds: 2));
+           yield SignedOut();
+      }
+
+      final Map current = Jwt.parseJwt(response.value);
+      print(current);
+      final role =current['role'];
+      if (role == "Normal User"){
+        yield SignedInAsNormalUser();
+      };
+      if (role == "University User"){
+        yield SignedInAsUnivUser();
+      };
+      if (role == "Admin User"){
+        yield SignedInAsAdminUser();
+      }
+      yield SignedOut();
+  
+    }
+
+    if (event is SignUpEvent) {
+      final User user = event.user;
+      // reaching to the backend
+      yield SignUpInprogress();
+      final response = await authRepository.signUp(user);
+
+      if (response.value == "Failure") {
+        yield AuthFailed(errorMsg: 'Operation failed please try again');
+        await Future.delayed(Duration(seconds: 2));
+           yield SignedOut();
+      }
+
+      final Map current = Jwt.parseJwt(response.value);
+     
+      yield SignedInAsNormalUser();
+      
+    }
+    if (event is SignOutEvent) {
+      final User user = event.user;
+      // reaching to the backend
+      yield SignOutInprogress();
+      final response = await authRepository.signOut(user);
+
+      if (response == "Failure") {
+        yield AuthFailed(errorMsg: 'Failed to log out');
+      yield SignedOut();
+      }
+    }
+
+    if (event is ForgotPasswordEvent) {
+      final String email = event.email;
+      // reaching to the backend
+      yield ForgotPasswordInprogress();
+      final response = await authRepository.forgotPassword(email);
+
+      if (response == "Failure") {
+        yield AuthFailed(errorMsg: 'Operation failed please try again');
+      }
+    }
+    if (event is ResetPasswordEvent) {
+      final String reset_link = event.reset_link;
+      final String password = event.password;
+      
+      // reaching to the backend
+      yield ResetPasswordInprogress();
+      final response = await authRepository.resetPassword(reset_link,password);
+
+      if (response == "Failure") {
+        yield ResetPasswordFailed(resetErrorMsg: 'Operation failed please try again');
+      }
+      yield ResetPasswordSuccess();
+    }
+
+
+}
+  
+}
--- temari_info_flutter/lib/presentation/signup/signup_screen.dart
+++ temari_info_flutter/lib/presentation/signup/signup_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:temari_info_flutter/presentation/admin/admin_univadd_screen.dart';
+import 'package:temari_info_flutter/presentation/home/home_screen.dart';
+import 'package:temari_info_flutter/presentation/login/login_screen.dart';
 import 'package:temari_info_flutter/presentation/shared/navBar_Widget.dart';
+import 'package:temari_info_flutter/auth/bloc/auth_bloc.dart';
+import 'package:temari_info_flutter/auth/bloc/auth_event.dart';
+import 'package:temari_info_flutter/auth/bloc/auth_state.dart';
+import 'package:temari_info_flutter/auth/data_providers/auth_provider.dart';
+import 'package:temari_info_flutter/auth/models/auth_model.dart';
+import 'package:temari_info_flutter/auth/repository/auth_repo.dart';
+import 'package:http/http.dart' as http;
+import 'package:temari_info_flutter/presentation/signup/signup_screen.dart';
+import 'package:temari_info_flutter/presentation/univ_admin/edit_univ_screen.dart';
 
 class Signup extends StatelessWidget {
   static const String routeName = "/signup";
+Signup({Key? key}) : super(key: key);
+
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+  final userameTextController = TextEditingController();
+  final confirmpasswordTextController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +49,7 @@ class Signup extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(10),
                   child: TextField(
-                    // controller: nameController,
+                   controller: userameTextController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'User Name',
@@ -37,7 +59,7 @@ class Signup extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.all(10),
                   child: TextField(
-                    // controller: emailController,
+                   controller: emailTextController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Email Address',
@@ -48,7 +70,7 @@ class Signup extends StatelessWidget {
                   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                   child: TextField(
                     obscureText: true,
-                    // controller: passwordController,
+                    controller: passwordTextController,
                     decoration: InputDecoration(
                       suffixIcon: const Padding(
                       padding: const EdgeInsets.only(right: 0),
@@ -62,7 +84,7 @@ class Signup extends StatelessWidget {
                   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                   child: TextField(
                     obscureText: true,
-                    // controller: confirmpasswordController,
+                    controller: confirmpasswordTextController,
                     decoration: InputDecoration(
                       suffixIcon: const Padding(
                       padding: const EdgeInsets.only(right: 0),
@@ -71,15 +93,60 @@ class Signup extends StatelessWidget {
                       labelText: 'Confirm Password',
                     ),
                   ),
-                ),              
-                Container(
+                ), BlocConsumer<AuthBloc, AuthState>(
+                listener: (ctx, authState) {
+                  if (authState is SignedInAsNormalUser) {
+                    Navigator.of(context).pushNamed(Home.routeName);
+                  };
+                },
+                builder: (ctx, authState) {
+                  Widget buttonChild = Text("Sign Up");
+                 
+                  if (authState is SignUpInprogress) {
+                    buttonChild = SizedBox(
+                      height: 4,
+                      width: 4,
+                      child: Row(children:[CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                      Text("Sign up in progress")
+                    ]));
+                  }
+  
+                  if (authState is AuthFailed) {
+                    buttonChild = Text(authState.errorMsg);
+                  }
+                  
+          final disabled = authState is SigninInprogress || authState is AuthFailed;
+
+                  return Container(
                   height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: ElevatedButton(                  
-                      child: Text('Sign Up'),
-                      onPressed: () {
-                      },
-                    )),
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child:ElevatedButton(
+                    onPressed: () {
+                      final authBloc = BlocProvider.of<AuthBloc>(context);
+
+                      authBloc.add(
+                        SignInEvent(user:
+                          User(username: userameTextController.text,
+                            email: emailTextController.text,
+                          password: passwordTextController.text
+                          ),
+                        ),
+                      );
+                    },
+                    child: buttonChild,
+                  ));
+                },
+              ),             
+                // Container(
+                //   height: 50,
+                //     padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                //     child: ElevatedButton(                  
+                //       child: Text('Sign Up'),
+                //       onPressed: () {
+                //       },
+                //     )),
                 Container(
                   child: Row(
                     children: <Widget>[
@@ -90,6 +157,8 @@ class Signup extends StatelessWidget {
                           style: TextStyle(fontSize: 15),
                         ),
                         onPressed: () {
+                        Navigator.of(context).pushNamed(Login.routeName);
+
                         },
                     ),
                       
